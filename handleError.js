07b8b0690782ae412c3249c280a3724375e68f09
Emma Shih <emma.shih@shopee.com>
2022-04-24 17:12:06 +0800

    extract success and error handle, revise as posts schema and model

---
+++ handleError.js
@@ -0,0 +1,18 @@
+const headers = require('./headers');
+const handleError = (res, err) => {
+  res.writeHead(400, headers);
+  let message = '';
+  if (err) {
+    message = err.message;
+  } else {
+    message = '欄位未填寫正確或無此ID';
+  }
+  res.write(
+    JSON.stringify({
+      status: 'false',
+      message,
+    })
+  );
+  res.end();
+};
+module.exports = handleError;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Room = require('./models/room');
+const headers = require('./headers');
+const handleSuccess = require('./handleSuccess.js');
+const handleError = require('./handleError.js');
+const Posts = require('./models/posts');
 
 dotenv.config({ path: './config.env' });
 
@@ -19,122 +22,49 @@ mongoose
     console.log(error);
   });
 
-const postSchema = new mongoose.Schema({});
-
-// Schema 開始
-const Post = mongoose.model('post', postSchema);
-
-// const roomSchema = new mongoose.Schema(
-//   {
-//     name: String,
-//     price: {
-//       type: Number,
-//       required: [true, '價格必填'],
-//     },
-//     rating: Number,
-//     createdAt: {
-//       type: Date,
-//       default: Date.now,
-//       select: false,
-//     },
-//   },
-//   { versionKey: false }
-// );
-
-// const Room = mongoose.model('Room', roomSchema);
-
-// Room.create({
-//   name: 'Whiteheaven 徵室友',
-//   price: 1050,
-//   rating: 3,
-// });
-// const testRoom = new Room(
-//   name: 'Queens Town公寓單人房',
-//   price: 1100,
-//   rating: 3,
-// });
-
-// testRoom
-//   .save()
-//   .then(() => {
-//     console.log('新增資料成功');
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 const requestListener = async (req, res) => {
   let body = '';
   req.on('data', (chunk) => {
     body += chunk;
   });
-  const headers = {
-    'Access-Control-Allow-Headers':
-      'Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-    'Content-Type': 'application/json',
-  };
-  console.log(req.url);
-  if (req.url == '/rooms' && req.method == 'GET') {
-    const rooms = await Room.find();
-    res.writeHead(200, headers);
-    res.write(
-      JSON.stringify({
-        status: 'success',
-        rooms,
-      })
-    );
+
+  if (req.url == '/posts' && req.method == 'GET') {
+    const allPosts = await Posts.find();
+    handleSuccess(res, allPosts);
     res.end();
-  } else if (req.url === '/rooms' && req.method === 'POST') {
+  } else if (req.url === '/posts' && req.method === 'POST') {
     req.on('end', async () => {
       try {
         const data = JSON.parse(body);
         console.log(data);
-        const newRoom = await Room.create({
-          name: data.name,
-          price: data.price,
-          rating: data.rating,
-        });
-        res.writeHead(200, headers);
-        res.write(
-          JSON.stringify({
-            status: 'success',
-            rooms: newRoom,
-          })
-        );
-
-        res.end();
+        if (data.content) {
+          const newPost = await Posts.create({
+            name: data.name,
+            content: data.content,
+            tags: data.tags,
+            type: data.type,
+          });
+          handleSuccess(res, newPost);
+        } else {
+          handleError(res);
+        }
       } catch (error) {
-        res.writeHead(400, headers);
-        res.write(
-          JSON.stringify({
-            status: 'false',
-            rooms: '欄位沒有正確填寫',
-            error: error,
-          })
-        );
-        console.log(error);
-        res.end();
+        handleError(res, err);
       }
     });
-  } else if (req.url.startWith('/rooms') && req.method === 'DELETE') {
+  } else if (req.url.startWith('/posts') && req.method === 'DELETE') {
     const id = req.url.split('/').pop();
-    await Post.findByIdAndDelete(id);
-    const rooms = await Room.deleteMany({});
+    await Posts.findByIdAndDelete(id);
+
+    const posts = await Posts.deleteMany({});
     res.writeHead(200, headers);
-    res.write(
-      JSON.stringify({
-        status: 'success ',
-        data: null,
-      })
-    );
+    handleSuccess(res, null);
     res.end();
   } else if (req.method === 'OPTIONS') {
     res.writeHead(200, headers);
     res.end();
   } else {
-    res.writeHead(400, headers);
+    res.writeHead(404, headers);
     res.write(
       JSON.stringify({
         status: 'false',
